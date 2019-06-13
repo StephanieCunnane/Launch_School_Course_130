a964a4992759204ffe7d1107c963858d53a09984
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-06-13 06:05:29 -0700

    Add code for given solution

--- lesson_1/todo_list.rb
+++ lesson_1/todo_list.rb
@@ -165,3 +165,32 @@ list.add(todo3)
 
 todo1.done!
 
+# Given solution
+class TodoList
+  # code omitted
+
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
+end
+
