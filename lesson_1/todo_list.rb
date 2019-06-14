5148a5fbef713b24ff749725462d94f4a8268768
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-06-14 09:24:38 -0700

    Refactor initial TodoList class

--- lesson_1/todo_list.rb
+++ lesson_1/todo_list.rb
@@ -194,3 +194,102 @@ class TodoList
   end
 end
 
+# Refactor initial TodoList class
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+  
+  attr_accessor :title, :description, :done
+  
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+  
+  def done!
+    self.done = true
+  end
+  
+  def done?
+    done
+  end
+  
+  def undone!
+    self.done = false
+  end
+  
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+  
+  def add(new_todo)
+    raise TypeError, "Can only add Todo objects" unless new_todo.instance_of?(Todo)
+    @todos << new_todo
+  end
+  
+  alias :<< :add
+  
+  def done!
+    @todos.each(&:done!)
+  end
+  
+  def done?
+    @todos.all?(&:done?)
+  end
+  
+  def first
+    @todos.first
+  end
+  
+  def item_at(idx)
+    @todos.fetch(idx)
+  end
+  
+  def last
+    @todos.last
+  end
+  
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+  
+  def mark_undone_at(idx)
+    item_at(idx).undone!
+  end
+  
+  def pop
+    @todos.pop
+  end
+  
+  def remove_at(idx)
+    item_at(idx)
+    @todos.delete_at(idx)
+  end
+  
+  def shift
+    @todos.shift
+  end
+  
+  def size
+    @todos.size
+  end
+  
+  def to_a
+    @todos.clone
+  end
+  
+  def to_s
+    todos = @todos.map { |todo| todo.to_s + "\n" }.join
+    "---- Today's Todos ----\n#{todos}" 
+  end
+end
